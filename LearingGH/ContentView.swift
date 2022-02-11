1a44f8e7d98e4e3c459cec0fcaf0827d8a6114ac
Tsuyoshi1988 <ms.2bpd.cch@gmail.com>
2022-02-11 21:47:59 +0900

    Initial Commit

---
+++ LearingGH/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearingGH
+//
+//  Created by 鎌田剛史 on 2022/02/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello,CodeCrew!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearingGH/LearingGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearingGHApp.swift
+//  LearingGH
+//
+//  Created by 鎌田剛史 on 2022/02/11.
+//
+
+import SwiftUI
+
+@main
+struct LearingGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
